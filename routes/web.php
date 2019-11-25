587f4115ba5dbdde9af33d25939f44a96cfc40e4
leesm002 <gmlsenfn34@naver.com>
2019-11-25 19:35:53 +0900

    routed

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,8 @@ Route::get('/', function () {
 });
 
 Route::get('/logged', 'LoggedController@index')->name('logged');
+
+//헤더부분에서 필요
+Route::get('/HH', 'HHController@index')->name('HH');
+Route::get('/Introduce', 'IntroduceController@index')->name('Introduce');
+Route::get('/QnA', 'QnAController@index')->name('QnA');
